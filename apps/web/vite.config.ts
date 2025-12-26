bcbd6e25d27ffc5d64a70b232fd3823993c9ef43
JinJieBeWater <cjj20030813@qq.com>
2025-12-27 05:44:47 +0800

    feat: :zap: 初始化

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,4 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
@@ -6,7 +6,11 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tanstackRouter({}), react(), VitePWA({
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({}),
+    react(),
+    VitePWA({
       registerType: "autoUpdate",
       manifest: {
         name: "dono",
@@ -16,7 +20,8 @@ export default defineConfig({
       },
       pwaAssets: { disabled: false, config: true },
       devOptions: { enabled: true },
-    })],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth";
 export * from "./todo";
+// oxlint-disable-next-line no-useless-empty-export
 export {};
